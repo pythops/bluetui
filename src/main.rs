b0b3da43be81f5a691a8f95e89d0461176c4ea31
pythops <contact@pythops.com>
2024-09-04 20:24:56 +0200

    use stdout instead of stderr for crossterm

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ async fn main() -> AppResult<()> {
 
     let config = Arc::new(Config::new());
     let mut app = App::new(config.clone()).await?;
-    let backend = CrosstermBackend::new(io::stderr());
+    let backend = CrosstermBackend::new(io::stdout());
     let terminal = Terminal::new(backend)?;
     let events = EventHandler::new(1_000);
     let mut tui = Tui::new(terminal, events);
