a299bafc386cb90c72512178339331801bcccd73
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-11 02:31:56 +0300

    fmt: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,28 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
-  outputs =
-    { self, nixpkgs }:
-    {
-      packages =
-        nixpkgs.lib.genAttrs
-          [
-            "x86_64-linux"
-            "aarch64-linux"
-          ]
-          (system: rec {
-            bluetui = nixpkgs.legacyPackages.${system}.callPackage ./package.nix { };
-            default = bluetui;
-          });
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    packages =
+      nixpkgs.lib.genAttrs
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+      ]
+      (system: rec {
+        bluetui = nixpkgs.legacyPackages.${system}.callPackage ./package.nix {};
+        default = bluetui;
+      });
 
-      devShells.x86_64-linux.default = 
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
+    devShells.x86_64-linux.default = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
       pkgs.mkShell {
         buildInputs = [
           self.packages.x86_64-linux.bluetui
         ];
       };
-    };
+  };
 }
