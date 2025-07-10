050273e10801b0be580edc8caad9e18cfc41ef4f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-11 02:51:13 +0300

    refactor: update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,35 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = {
-    self,
     nixpkgs,
-  }: {
-    packages =
-      nixpkgs.lib.genAttrs
-      [
-        "x86_64-linux"
-        "aarch64-linux"
-      ]
-      (system: rec {
-        bluetui = nixpkgs.legacyPackages.${system}.callPackage ./package.nix {};
-        default = bluetui;
-      });
-
-    devShells.x86_64-linux.default = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-      pkgs.mkShell {
-        buildInputs = [
-          self.packages.x86_64-linux.bluetui
-        ];
-      };
-  };
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            dbus
+            pkg-config
+            rust-bin.stable.latest.default
+          ];
+        };
+        packages = rec {
+          default = bluetui;
+          bluetui = nixpkgs.legacyPackages.${system}.callPackage ./package.nix {};
+        };
+      }
+    );
 }
